2bc41601320a52ab9f66f123846691abc2fed9ec
JuanMosquera <jm1128un@gmail.com>
2019-08-20 12:12:50 -0500

    pregunta 4

---
+++ dinamica.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, const char* argv[]){
+   
+    int cantidad=10;
+    int *x;
+
+    //printf("Cantidad de valores del array: ");
+    //scanf("%d",&cantidad);
+
+    x = (int*)malloc(sizeof(int)*cantidad);
+
+    for(int i=0;i<cantidad;i++)
+    {
+        //printf("Numero: ");
+        //scanf("%d",&x[i]);
+        x[i]=1;
+    }
+
+    //Visualizar Datos
+    for(int i=0;i<cantidad;i++)
+    {
+        printf(" %d",x[i]);
+    }
+
+    return 0;
+}
