b4f9af2d81a53ad462ec1b7cea665992bd7b3076
JuanMosquera <jm1128un@gmail.com>
2019-08-20 12:53:29 -0500

    repuesta 8

--- dinamica.c
+++ dinamica.c
@@ -7,28 +7,37 @@ int main(int argc, const char* argv[]){
     int cantidad=100;
     int *y;
     int *p;
+    int i,x;
 
-    //printf("Cantidad de valores del array: ");
-    //scanf("%d",&cantidad);
+    printf("Cantidad de valores del array: ");
+    scanf("%d",&cantidad);
 
     y = (int*)malloc(sizeof(int)*cantidad);
 
-    for(int i=0;i<cantidad;i++)
+    for( i=0;i<cantidad;i++)
     {
-        //printf("Numero: ");
-        //scanf("%d",&x[i]);
-        y[i]=1;
-        if(i == 50){
-        	y[i]=*p;
-        }
+        printf("Numero: ");
+        scanf("%d",&y[i]);
+        
     }
+    x= cantidad;
+    printf("Cantidad de valores del array: ");
+    scanf("%d",&cantidad);
     
+    y = (int*)realloc(y,sizeof(int)*cantidad);
 
+     for( i=x;i<cantidad;i++)
+    {
+        printf("Numero: ");
+        scanf("%d",&y[i]);
+        
+    }
     //Visualizar Datos
-    for(int i=0;i<=cantidad;i++)
+    for(int i=0;i<cantidad;i++)
     {
         printf(" %d",y[i]);
     }
 
+    free(y);
     return 0;
 }
